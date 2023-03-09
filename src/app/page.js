d348ce862fd541bde8333e16a71cf2425ecd5d4e
xukooru <xukooru@gmail.com>
2023-03-09 12:54:11 +0500

    Tailwind Install

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main >
-      <h1>IMDB CLone</h1>
+      <h1 className='text-red-700 font-black'>IMDB CLone</h1>
     </main>
   )
 }
